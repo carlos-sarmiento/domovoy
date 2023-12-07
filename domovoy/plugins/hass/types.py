9095886374a9e5a2d56239c3110d89880e612597
Carlos G. Sarmiento <carlos@sarmiento.io>
2023-12-07 02:05:25 -0600

    Add shorter types for Hass plugin

--- domovoy/plugins/hass/types.py
+++ domovoy/plugins/hass/types.py
@@ -7,4 +7,12 @@ PrimitiveHassApiValue = int | float | str | bool | datetime.datetime
 
 HassApiValue = PrimitiveHassApiValue | list["HassApiValue"] | dict[str, Union["HassApiValue", None]]
 
+HassApiValueOrNone = HassApiValue | None
+
 HassApiDataDict = dict[str, HassApiValue | None]
+
+HassData = HassApiDataDict
+
+HassValue = HassApiValue
+
+HassValueOrNone = HassApiValueOrNone
