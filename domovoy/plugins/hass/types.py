1a76fd92a76f9e2101dda216722bf44344db7344
Carlos G. Sarmiento <carlos@sarmiento.io>
2024-09-14 00:01:59 -0500

    Add improvement to entities typing

--- domovoy/plugins/hass/types.py
+++ domovoy/plugins/hass/types.py
@@ -12,3 +12,17 @@ HassData = dict[str, HassApiValue | None]
 HassValue = HassApiValue | None
 
 HassValueStrict = HassApiValue
+
+
+class HassEntity:
+    def __init__(self, entity_id: str) -> None:
+        self._entity_id: str = entity_id
+
+    def __str__(self) -> str:
+        return self.value()
+
+    def __repl__(self) -> str:
+        return f"HassEntity('{self._entity_id}')"
+
+    def value(self) -> str:
+        return self._entity_id
