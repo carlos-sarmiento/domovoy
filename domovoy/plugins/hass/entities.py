afeaa7d43b049eeaad217b12928970c2350a1ce8
Carlos G. Sarmiento <carlos@sarmiento.io>
2025-01-02 01:01:49 -0600

    Added Domain Specific return types

--- domovoy/plugins/hass/entities.py
+++ domovoy/plugins/hass/entities.py
@@ -1,53 +1,50 @@
 import datetime
+import inspect
 from io import StringIO
 from pathlib import Path
-from typing import Literal
 
 from domovoy.core.configuration import get_main_config
+from domovoy.plugins.hass import domains as defined_domains_cls
 from domovoy.plugins.hass.types import EntityID
 
+defined_domain_classes = set()
+for name, obj in inspect.getmembers(defined_domains_cls):
+    if inspect.isclass(obj):
+        defined_domain_classes.add(name)
 
-class HassSyntheticPlatformEntities:
-    __platform: str
 
-    def __init__(self, *, platform: str, return_entity_cls: bool) -> None:
-        self.__platform = platform
-        self.__return_entity_cls = return_entity_cls
+class HassSyntheticDomain:
+    __domain: str
 
-    def __getattr__(self, entity: str) -> EntityID | str:
+    def __init__(self, *, domain: str) -> None:
+        self.__domain = domain
+
+    def __getattr__(self, entity: str) -> EntityID:
         entity = entity.removeprefix("_")
-        entity_id = f"{self.__platform}.{entity}"
+        entity_id = f"{self.__domain}.{entity}"
 
-        if self.__return_entity_cls:
-            return EntityID(entity_id)
-
-        return entity_id
+        return EntityID(entity_id)
 
 
-class HassSyntheticPlatforms:
-    __defined_domains: dict[str, HassSyntheticPlatformEntities]
+class HassSyntheticDomains:
+    __defined_domains: dict[str, HassSyntheticDomain]
 
-    def __init__(self, *, return_entity_cls: bool) -> None:
+    def __init__(self) -> None:
         self.__defined_domains = {}
-        self.__return_entity_cls = return_entity_cls
 
-    def __getattr__(self, name: str) -> HassSyntheticPlatformEntities:
+    def __getattr__(self, name: str) -> HassSyntheticDomain:
         if name not in self.__defined_domains:
-            self.__defined_domains[name] = HassSyntheticPlatformEntities(
-                platform=name,
-                return_entity_cls=self.__return_entity_cls,
+            self.__defined_domains[name] = HassSyntheticDomain(
+                domain=name,
             )
 
         return self.__defined_domains[name]
 
-    def __call__(self, entity_id: str) -> EntityID | str:
-        if self.__return_entity_cls:
-            return EntityID(entity_id)
-
-        return entity_id
+    def __call__(self, entity_id: str) -> EntityID:
+        return EntityID(entity_id)
 
 
-entities = HassSyntheticPlatforms(return_entity_cls=True)
+entities = HassSyntheticDomains()
 
 
 def __to_camel_case(snake_str: str) -> str:
@@ -55,7 +52,7 @@ def __to_camel_case(snake_str: str) -> str:
 
 
 def generate_stub_file_for_synthetic_entities(
-    platforms: dict[str, set[str]],
+    domains: dict[str, set[str]],
     destination: str,
 ) -> None:
     with Path(destination).open("w") as text_file:
@@ -63,44 +60,46 @@ def generate_stub_file_for_synthetic_entities(
         text_file.write(f"# Generated on {now.isoformat()}\n\n")
         text_file.write("# ruff: noqa\n\n")
 
-        text_file.write("from domovoy.plugins.hass.types import EntityID\n\n")
+        text_file.write("from domovoy.plugins.hass.types import EntityID\n")
+        text_file.write("from domovoy.plugins.hass.domains import *\n\n")
 
-        text_file.write(__build_class_hierarchy(platforms, "Entity", "EntityID"))
+        text_file.write(__build_class_hierarchy(domains))
 
         text_file.write(
-            "entities: HassSyntheticPlatformsEntity = ...\n\n",
+            "entities: HassSyntheticDomains = ...\n\n",
         )
 
 
 def __build_class_hierarchy(
-    platforms: dict[str, set[str]],
-    postfix: str,
-    return_type: Literal["EntityID", "str"],
+    domains: dict[str, set[str]],
 ) -> str:
+    return_type = "EntityID"
+
     text_file = StringIO()
 
-    postfix = postfix.capitalize()
-
-    text_file.write(f"class HassSyntheticPlatforms{postfix}:\n")
+    text_file.write("class HassSyntheticDomains:\n")
     text_file.write(
         "    def __init__(self) -> None: ...\n\n",
     )
     text_file.write(
         f"    def __call__(self, entity_id : str) -> {return_type}: ...\n\n",
     )
-    platform_to_class: dict[str, str] = {}
+    domain_to_class: dict[str, str] = {}
 
-    for platform, _entities in sorted(platforms.items()):
-        class_name = f"HassSynthetic{__to_camel_case(platform)}Platform{postfix}"
-        platform_to_class[platform] = class_name
-        text_file.write(f"    {platform}: {class_name}\n")
+    for domain, _entities in sorted(domains.items()):
+        class_name = f"HassSynthetic{__to_camel_case(domain)}Domain"
+        domain_to_class[domain] = class_name
+        text_file.write(f"    {domain}: {class_name}\n")
 
     text_file.write("\n\n")
 
-    for platform, entities in sorted(platforms.items()):
+    for domain, entities in sorted(domains.items()):
         text_file.write(
-            f"class HassSynthetic{__to_camel_case(platform)}Platform{postfix}:\n",
+            f"class HassSynthetic{__to_camel_case(domain)}Domain:\n",
         )
+
+        entity_class_name = f"{__to_camel_case(domain)}Entity"
+        return_type_for_domain = entity_class_name if entity_class_name in defined_domain_classes else return_type
 
         for entity_base in sorted(entities):
             field_name = entity_base
@@ -108,7 +107,7 @@ def __build_class_hierarchy(
                 field_name = "_" + entity_base
 
             text_file.write(
-                f"    {field_name} : {return_type} = ...\n",
+                f"    {field_name} : {return_type_for_domain} = ...\n",
             )
 
         text_file.write("\n\n")
